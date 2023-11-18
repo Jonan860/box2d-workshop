927937eaf3b18bbd6c3bf52e4a22269a7cebaec8
Jonathan Andersson <jonathan.andersson@knowit.se>
2023-11-18 16:12:59 +0100

    creation size task complete

--- workshop-game/main.cpp
+++ workshop-game/main.cpp
@@ -13,11 +13,41 @@
 
 #include "box2d/box2d.h"
 #include <set>
+#include <vector>
 
 // GLFW main window pointer
 GLFWwindow* g_mainWindow = nullptr;
 
 b2World* g_world;
+
+class Character {
+public:
+    Character(int p_size, float x, float y)
+    {
+        size = p_size;
+        b2PolygonShape box_shape;
+        box_shape.SetAsBox(size / 500.0f, size / 500.0f);
+        b2FixtureDef box_fd;
+        box_fd.shape = &box_shape;
+        box_fd.density = 20.f;
+        box_fd.friction = 0.1f;
+        b2BodyDef box_bd;
+        box_bd.userData.pointer = (uintptr_t)this;
+        box_bd.type = b2_dynamicBody;
+        box_bd.position.Set(x, y);
+        body = g_world->CreateBody(&box_bd);
+        body->CreateFixture(&box_fd);
+    }
+    virtual Character::~Character()
+    {
+        g_world->DestroyBody(body);
+    }
+private:
+    int size;
+    b2Body* body;
+};
+
+std::vector<std::unique_ptr<Character>> characters;
 
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -46,7 +76,8 @@ void MouseButtonCallback(GLFWwindow* window, int32 button, int32 action, int32 m
     b2Vec2 pw = g_camera.ConvertScreenToWorld(ps);
 
     if (action == GLFW_PRESS) {
-        b2Body* box;
+        characters.push_back(std::make_unique<Character>(yd, pw.x, pw.y));
+       /* b2Body* box;
         b2PolygonShape box_shape;
         box_shape.SetAsBox(1.0f, 1.0f);
         b2FixtureDef box_fd;
@@ -57,9 +88,13 @@ void MouseButtonCallback(GLFWwindow* window, int32 button, int32 action, int32 m
         box_bd.type = b2_dynamicBody;
         box_bd.position.Set(pw.x, pw.y);
         box = g_world->CreateBody(&box_bd);
-        box->CreateFixture(&box_fd);
+        box->CreateFixture(&box_fd);*/
     }
 }
+
+
+
+
 
 int main()
 {
