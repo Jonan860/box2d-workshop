f1913b512a7840d173c2fc5d4dab6a27581e6a2d
Jonathan Andersson <jonathan.andersson@knowit.se>
2023-11-27 18:54:47 +0100

    Game done

--- workshop-game/main.cpp
+++ workshop-game/main.cpp
@@ -14,14 +14,65 @@
 #include "box2d/box2d.h"
 #include <set>
 #include <vector>
-
+#include <iostream>
+#include <cstdlib>   // For rand() and srand()
+#include <ctime> 
 // GLFW main window pointer
-GLFWwindow* g_mainWindow = nullptr;
-
-b2World* g_world;
-std::set<b2Body*> to_delete;
 
 
+class GameObject;
+class Obstacle;
+
+
+class TheGame {
+public:
+    const float PIXELS_PER_UNIT = 20.0f;
+    float roomHeight = g_camera.m_height / PIXELS_PER_UNIT;
+    float roomWidth = g_camera.m_height / PIXELS_PER_UNIT;
+    GLFWwindow* g_mainWindow = nullptr;
+    b2World* world = nullptr;// g_world;
+    std::set<b2Body*> to_delete;
+    float stepsUntilNextObstacleCreation = 10 * 60;
+    float stepsBetweenObstaclesCreation = 10 * 60;
+    float obstacleGapLength = 10.0f;
+    float obstacleGapCeil;
+    float obstacleGapFloor;
+    void generateNewGap() {
+        obstacleGapFloor = (float)(std::rand() % (int)(roomHeight - obstacleGapLength + 4.0f));
+        obstacleGapCeil = obstacleGapFloor + obstacleGapLength;
+    }
+    std::vector<std::unique_ptr<GameObject>> all_objects;
+    void createObstacle(float floor, float ceil) {
+        b2Body* body;
+        b2PolygonShape box_shape;
+        box_shape.SetAsBox(1.0f, (ceil - floor) / 2.0f);
+        b2FixtureDef box_fd;
+        box_fd.shape = &box_shape;
+        b2BodyDef box_bd;
+        box_bd.type = b2_kinematicBody;
+        box_bd.position.Set(roomWidth, (ceil + floor) / 2.0f);
+        body = world->CreateBody(&box_bd);
+        body->CreateFixture(&box_fd);
+        body->SetLinearVelocity(b2Vec2(-5.0f, 0.0f));
+        all_objects.emplace_back(std::make_unique<Obstacle>(body));
+    }
+
+    void CreateObstacles() {
+        generateNewGap();
+        
+        createObstacle(0.0f, obstacleGapFloor);
+        createObstacle(obstacleGapCeil, roomHeight + 5.0f);
+    }
+    void Update() {
+        stepsUntilNextObstacleCreation -= 1;
+        if (stepsUntilNextObstacleCreation == 0) {
+            stepsUntilNextObstacleCreation = stepsBetweenObstaclesCreation;
+            CreateObstacles();
+        }
+    }
+};
+
+TheGame theGame;
 class GameObject
 {
 public:
@@ -29,7 +80,7 @@ public:
     GameObject::~GameObject()
     {
         if (body != nullptr) {
-            g_world->DestroyBody(body);
+            theGame.world->DestroyBody(body);
         }
     }
 
@@ -50,8 +101,19 @@ private:
     b2Body* body;
 };
 
-std::vector<std::unique_ptr<GameObject>> all_objects;
 
+class Obstacle : public GameObject {
+public:
+    Obstacle(b2Body* body_p) : GameObject(body_p) {};
+
+    virtual void OnMousePress(int x, int y) override {};
+    virtual void OnKeyPress(int key, int scancode, int action, int mods) override {};
+    virtual void Update() override {
+        if (GetBody()->GetPosition().x <= -30) {
+            shouldDelete = true;
+        }
+    };
+};
 
 b2Body* GameObject::GetBody() const
 {
@@ -68,15 +130,16 @@ public:
 
     void OnKeyPress(int key, int scancode, int action, int mods) override
     {
-        if (key == GLFW_KEY_UP) 
-        {
+        if(key == GLFW_KEY_UP) {
             b2Vec2 velocityVector = GetBody()->GetLinearVelocity();
             if (std::abs(velocityVector.x) + std::abs(velocityVector.y) < 0.1) {
                 GetBody()->ApplyLinearImpulseToCenter(b2Vec2(0.0f, 500.0f), true);
-            }
+            }       
         }
     }
 };
+
+
 
 class Character {
 public:
@@ -93,7 +156,7 @@ public:
         box_bd.userData.pointer = (uintptr_t)this;
         box_bd.type = b2_dynamicBody;
         box_bd.position.Set(x, y);
-        body = g_world->CreateBody(&box_bd);
+        body = theGame.world->CreateBody(&box_bd);
         body->CreateFixture(&box_fd);
     }
 
@@ -106,7 +169,7 @@ public:
 
     virtual Character::~Character()
     {
-        g_world->DestroyBody(body);
+        theGame.world->DestroyBody(body);
     }
     bool shouldDelete = false;
 private:
@@ -138,7 +201,19 @@ public:
 
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    for (auto& game_object : all_objects) {
+    float grav = 0.0f;
+    switch (key) {
+    case GLFW_KEY_1 :
+        grav = 1.0f; break;
+    case GLFW_KEY_2 :
+        grav = 10.0f; break;
+    case GLFW_KEY_3:
+        grav = 20.0f; break;
+    }
+    if (grav != 0.0f) {
+        theGame.world->SetGravity(b2Vec2(0.0f, -grav));
+    }
+    for (auto& game_object : theGame.all_objects) {
         // code for keys here https://www.glfw.org/docs/3.3/group__keys.html
         // and modifiers https://www.glfw.org/docs/3.3/group__mods.html
         game_object->OnKeyPress(key, scancode, action, mods);
@@ -160,13 +235,13 @@ void MouseButtonCallback(GLFWwindow* window, int32 button, int32 action, int32 m
     // action is either GLFW_PRESS or GLFW_RELEASE
     double xd, yd;
     // get the position where the mouse was pressed
-    glfwGetCursorPos(g_mainWindow, &xd, &yd);
+    glfwGetCursorPos(theGame.g_mainWindow, &xd, &yd);
     b2Vec2 ps((float)xd, (float)yd);
     // now convert this position to Box2D world coordinates
     b2Vec2 pw = g_camera.ConvertScreenToWorld(ps);
 
     if (action == GLFW_PRESS) {
-        characters.push_back(std::make_unique<Character>(yd, pw.x, pw.y));
+       // characters.push_back(std::make_unique<Character>(yd, pw.x, pw.y));
        /* b2Body* box;
         b2PolygonShape box_shape;
         box_shape.SetAsBox(1.0f, 1.0f);
@@ -177,7 +252,7 @@ void MouseButtonCallback(GLFWwindow* window, int32 button, int32 action, int32 m
         b2BodyDef box_bd;
         box_bd.type = b2_dynamicBody;
         box_bd.position.Set(pw.x, pw.y);
-        box = g_world->CreateBody(&box_bd);
+        box = theGame.world->CreateBody(&box_bd);
         box->CreateFixture(&box_fd);*/
     }
 }
@@ -193,20 +268,20 @@ int main()
         return -1;
     }
 
-    g_mainWindow = glfwCreateWindow(g_camera.m_width, g_camera.m_height, "My game", NULL, NULL);
+    theGame.g_mainWindow = glfwCreateWindow(g_camera.m_width, g_camera.m_height, "My game", NULL, NULL);
 
-    if (g_mainWindow == NULL) {
+    if (theGame.g_mainWindow == NULL) {
         fprintf(stderr, "Failed to open GLFW g_mainWindow.\n");
         glfwTerminate();
         return -1;
     }
 
     // Set callbacks using GLFW
-    glfwSetMouseButtonCallback(g_mainWindow, MouseButtonCallback);
-    glfwSetKeyCallback(g_mainWindow, KeyCallback);
-    glfwSetCursorPosCallback(g_mainWindow, MouseMotionCallback);
+    glfwSetMouseButtonCallback(theGame.g_mainWindow, MouseButtonCallback);
+    glfwSetKeyCallback(theGame.g_mainWindow, KeyCallback);
+    glfwSetCursorPosCallback(theGame.g_mainWindow, MouseMotionCallback);
 
-    glfwMakeContextCurrent(g_mainWindow);
+    glfwMakeContextCurrent(theGame.g_mainWindow);
 
     // Load OpenGL functions using glad
     int version = gladLoadGL(glfwGetProcAddress);
@@ -214,13 +289,13 @@ int main()
     // Setup Box2D world and with some gravity
     b2Vec2 gravity;
     gravity.Set(0.0f, -10.0f);
-    g_world = new b2World(gravity);
+    theGame.world = new b2World(gravity);
 
     // Create debug draw. We will be using the debugDraw visualization to create
     // our games. Debug draw calls all the OpenGL functions for us.
     g_debugDraw.Create();
-    g_world->SetDebugDraw(&g_debugDraw);
-    CreateUI(g_mainWindow, 20.0f /* font size in pixels */);
+    theGame.world->SetDebugDraw(&g_debugDraw);
+    CreateUI(theGame.g_mainWindow, 20.0f /* font size in pixels */);
 
 
     // Some starter objects are created here, such as the ground
@@ -228,7 +303,7 @@ int main()
     b2EdgeShape ground_shape;
     ground_shape.SetTwoSided(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f));
     b2BodyDef ground_bd;
-    ground = g_world->CreateBody(&ground_bd);
+    ground = theGame.world->CreateBody(&ground_bd);
     ground->CreateFixture(&ground_shape, 0.0f);
 
     //Jonathans Dominos
@@ -246,7 +321,7 @@ int main()
         b2BodyDef bd;
         bd.type = b2_dynamicBody;
         bd.position.Set(-5.0f + 1.0f * i, 0.01f);
-        b2Body* body = g_world->CreateBody(&bd);
+        b2Body* body = theGame.world->CreateBody(&bd);
         body->SetAngularVelocity(1.0f);
         body->CreateFixture(&fd);
     }
@@ -258,19 +333,20 @@ int main()
     box_fd.shape = &box_shape;
     box_fd.density = 20.0f;
     box_fd.friction = 0.1f;
+    box_fd.restitution = 1.0f;
     b2BodyDef box_bd;
     box_bd.type = b2_dynamicBody;
-    box_bd.position.Set(8.0f, 11.25f);
-    box = g_world->CreateBody(&box_bd);
+    box_bd.position.Set(-20.0f, 10.0f);
+    box = theGame.world->CreateBody(&box_bd);
     box->CreateFixture(&box_fd);
 
     //std::make_unique<Square>(box);
     
-    all_objects.push_back(std::make_unique<Square>(box));
+    theGame.all_objects.emplace_back(std::make_unique<Square>(box));
     
 
     MyCollisionListener collision;
-    g_world->SetContactListener(&collision);
+    theGame.world->SetContactListener(&collision);
 
     // This is the color of our background in RGB components
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -280,15 +356,15 @@ int main()
     std::chrono::duration<double> sleepAdjust(0.0);
 
     // Main application loop
-    while (!glfwWindowShouldClose(g_mainWindow)) {
+    while (!glfwWindowShouldClose(theGame.g_mainWindow)) {
         // Use std::chrono to control frame rate. Objective here is to maintain
         // a steady 60 frames per second (no more, hopefully no less)
         std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
-        glfwGetWindowSize(g_mainWindow, &g_camera.m_width, &g_camera.m_height);
+        glfwGetWindowSize(theGame.g_mainWindow, &g_camera.m_width, &g_camera.m_height);
 
         int bufferWidth, bufferHeight;
-        glfwGetFramebufferSize(g_mainWindow, &bufferWidth, &bufferHeight);
+        glfwGetFramebufferSize(theGame.g_mainWindow, &bufferWidth, &bufferHeight);
         glViewport(0, 0, bufferWidth, bufferHeight);
 
         // Clear previous frame (avoid creates shadows)
@@ -310,10 +386,10 @@ int main()
         uint32 flags = 0;
         flags += b2Draw::e_shapeBit;
         g_debugDraw.SetFlags(flags);
-
+        
         // When we call Step(), we run the simulation for one frame
         float timeStep = 60 > 0.0f ? 1.0f / 60 : float(0.0f);
-        g_world->Step(timeStep, 8, 3);
+        theGame.world->Step(timeStep, 8, 3);
         
         characters.erase(
             std::remove_if(characters.begin(), characters.end(), [](const auto& character) {
@@ -322,7 +398,7 @@ int main()
             characters.end()
         );
 
-        for (b2Body* b = g_world->GetBodyList(); b; b = b->GetNext())
+        for (b2Body* b = theGame.world->GetBodyList(); b; b = b->GetNext())
         {
             if (b->GetType() == b2_dynamicBody)
             {
@@ -330,7 +406,7 @@ int main()
                 while (edge != nullptr) {
                     if (edge->contact->IsTouching()) {
                         if (edge->other->GetType() == b2_dynamicBody) {
-                            to_delete.insert(edge->other);
+                            theGame.to_delete.insert(edge->other);
                         }
                     }
                     edge = edge->next;
@@ -344,17 +420,17 @@ int main()
                 , all_objects.end()
             );*/
 
-        for (auto element : to_delete) {
-           g_world->DestroyBody(element);
+        for (auto element : theGame.to_delete) {
+           theGame.world->DestroyBody(element);
         }
-        to_delete.clear();
+        theGame.to_delete.clear();
       
         // Render everything on the screen
-        g_world->DebugDraw();
+        theGame.world->DebugDraw();
         g_debugDraw.Flush();
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(g_mainWindow);
+        glfwSwapBuffers(theGame.g_mainWindow);
 
         // Process events (mouse and keyboard) and call the functions we
         // registered before.
@@ -376,13 +452,13 @@ int main()
         // Compute the sleep adjustment using a low pass filter
         sleepAdjust = 0.9 * sleepAdjust + 0.1 * (target - frameTime);
 
-        
+        theGame.Update();
     }
 
     // Terminate the program if it reaches here
     glfwTerminate();
     g_debugDraw.Destroy();
-    delete g_world;
+    delete theGame.world;
 
     return 0;
 }
